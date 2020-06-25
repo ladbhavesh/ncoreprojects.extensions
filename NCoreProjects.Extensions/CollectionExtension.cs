b18f062323b8d626452454d2f935c01ad1ecf627
Bhavesh Lad <lad.bhavesh@gmail.com>
2020-06-25 19:22:40 +0530

    initial commit for cs file

---
+++ NCoreProjects.Extensions/CollectionExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NCoreProjects.Extensions
+{
+    public static class CollectionExtension
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        {
+            if (collection == null) return true;
+
+            return collection.Any();
+        }
+
+        public static bool SafeAny<T>(this IEnumerable<T> collection)
+        {
+            return collection.IsNullOrEmpty();
+        }
+
+        public static List<T> ToSafeList<T>(this IEnumerable<T> collection) 
+        {
+            if (collection.IsNullOrEmpty())
+                return new List<T>();
+
+            return collection.ToList();
+        }
+
+        public static IEnumerable<T> ToSafeEnumerable<T>(this IEnumerable<T> collection)
+        {
+            if (collection.IsNullOrEmpty())
+                return Enumerable.Empty<T>();
+
+            return collection;
+        }
+    }
+}
---
+++ NCoreProjects.Extensions/StringExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NCoreProjects.Extensions
+{
+    public static class StringExtension
+
+
+    {
+        public static bool IsNull(this string value)
+        {
+            return value == null;
+        }
+
+        public static bool IsNullOrEmpty(this string value)
+        {
+                        return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        public static bool SafeEquals(this string source, string destination, StringComparison stringComparison = StringComparison.InvariantCultureIgnoreCase)
+        {
+            if (source == null && destination == null) return true;
+
+            if (source == null) return false;
+
+            if (destination == null) return false;
+
+            return source.Equals(destination, stringComparison);
+
+        }
+
+        public static void ShouldNotBeNull<T>(this T source, string paramName) where T :class
+        {
+            if (source == null)
+                throw new ArgumentNullException(paramName);
+        }
+    }
+}
